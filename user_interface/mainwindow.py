d8df32efeac382a371de30e2c279905e04a8cbe4
Devasia Manuel <devasia1000@gmail.com>
2015-11-01 17:00:48 +0000

    Show maximized on startup

--- user_interface/mainwindow.py
+++ user_interface/mainwindow.py
@@ -141,5 +141,5 @@ class Ui_MainWindow(QMainWindow):
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     ex = Ui_MainWindow()
-    ex.show()
+    ex.showMaximized()
     sys.exit(app.exec_())
